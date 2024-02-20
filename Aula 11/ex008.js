9b26782acd671d7c38eb4d49f72c030dd1686ad5
Ronaldo Junior <dev.ronaldojunior@gmail.com>
2024-02-20 17:23:51 -0300

    aula de condições

---
+++ Aula 11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 56
+console.log(`A sua velocidade é de ${velocidade}Km/h`)
+if (velocidade > 60){
+    console.log('Você ultrapassou o limite de 60Km/h')
+    console.log('Você foi multado!')
+}
+console.log('Dirija sempre com o cinto de segurança!')
