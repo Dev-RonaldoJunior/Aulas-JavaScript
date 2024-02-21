8985cece8dddb6cf2b93989e1be8f27f4144c715
Ronaldo Junior <dev.ronaldojunior@gmail.com>
2024-02-21 11:16:06 -0300

    condições multiplas
    pegando a hora do sistema

--- Aula 12/ex011.js
+++ Aula 12/ex011.js
@@ -1,9 +1,10 @@
 var idade = 66
+console.log(`Você tem ${idade} anos`)
 
 if (idade < 16) {
-    console.log('Não Vota!')
+    console.log('Você não Vota!')
 }else if (idade < 18 || idade > 65) {
-    console.log( 'Voto Opcional!')
+    console.log( 'Seu voto é Opcional!')
 }else {
-    console.log('Voto Obrigatorio!')
+    console.log('Seu voto é Obrigatorio!')
 }
