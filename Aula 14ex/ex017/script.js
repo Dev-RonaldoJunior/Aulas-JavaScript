3e76928f682dbe46a778c2480ec63e0b78ae0c81
Ronaldo Junior <dev.ronaldojunior@gmail.com>
2024-02-22 17:11:26 -0300

    adicionando elementos HTML
    selec e options

--- Aula 14ex/ex017/script.js
+++ Aula 14ex/ex017/script.js
@@ -1,11 +1,18 @@
 function gerar() {
-    var num = Number(document.getElementById('num').value)
-    var c = 0
-    var divr = document.getElementById('res')
-    divr.innerHTML = ''
-    while (c <= 10) {
-        var resp = num * c
-        divr.innerText += ` ${num} x ${c} = ${resp}`
-        c += 1
+    var num = document.getElementById('num')
+    var tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('[ERRO] Insira um numero!')
+    } else {
+        var n = Number(num.value)
+        var c = 0
+        tab.innerHTML = ''
+        while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}` //Usa em PHP
+            tab.appendChild(item)
+            c++
+        }
     }
 }
