e949055c0562da7f35a5f686b3ea6d555ac45246
Ronaldo Junior <dev.ronaldojunior@gmail.com>
2024-02-22 10:52:57 -0300

    estruturas de repetições
    Aula de estruturas de repetições

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1 
+
+/*
+while(c <= 6) {
+    console.log(`Passo ${c}`)
+    c += 1
+}
+*/
+
+
+do { 
+    console.log(`Passo ${c}`)
+    c ++
+}while (c <= 6)
+
+//1ª forma = estrutura de repetição com teste logico no inicio
+//  1º testa se a condição é verdadeira e depois executa o comando
+//
+//2ª forma = estrutura de repetição com teste logico no final
+//  1º executa o comando e depois testa se a condição é verdadeira
+//
